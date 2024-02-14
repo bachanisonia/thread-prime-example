f920f5ec450a3ba1dafbd2f4cb2ebde2c43cc8fe
Sonia <bachani.sonia@gmail.com>
2024-02-14 10:18:07 +0000

    Initial Commit.

---
+++ thread-prime-example/src/main/java/com/learnjava/corejava/ThreadPrimeExample.java
@@ -0,0 +1,105 @@
+package com.learnjava.corejava;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ThreadPrimeExample {
+
+	public static void main(String[] args) {
+		
+		List<Thread> threads = new ArrayList<>();
+		
+		Runnable statusReporter = new Runnable() {
+			
+			@Override
+			public void run() {
+			
+				try {
+					while(true) {		
+						// Pause for 5 secs
+						Thread.sleep(5000);
+						printThreads(threads);
+					}
+				}
+				catch(InterruptedException e) {
+					e.printStackTrace();
+					System.out.println("Interrupted Exception !!!");
+				}
+			}
+		};
+		
+		Thread reporterThread = new Thread(statusReporter);
+		reporterThread.setDaemon(true);
+		reporterThread.start();
+		
+		while(true) {
+			
+			Scanner sc = new Scanner(System.in);
+			System.out.println("\n I can tell you the nth prime number. Enter n: ");
+			
+			int n = sc.nextInt();
+			if (n==0) break;
+			
+			Runnable r = new Runnable() {
+				
+				@Override
+				public void run() {
+					
+					int number = PrimeNumberUtil.calculatePrime(n);
+					System.out.println("\n Value of the " + n + "th prime number is : " +  number);
+					
+				}
+			};	
+			
+			Thread t = new Thread(r);
+			threads.add(t);
+			t.setDaemon(true); // Turning it into a daemon thread
+			t.start();
+			
+		}
+ 
+	}
+	
+	public static void printThreads(List<Thread> threads) {
+		
+		System.out.println("\n Thread Status : ");
+		
+		for(Thread s : threads) {
+			System.out.println(s.getState());
+		}
+	}
+
+	
+	
+}
+
+
+class PrimeNumberUtil {
+	
+	private static int outputNo;
+	
+	public static int calculatePrime(int inputNo) {
+		
+		int numOfPrimesFound = 0;
+		int i;
+		int number = 1;
+		
+		while (numOfPrimesFound < inputNo) {
+			
+			number++;
+			
+			for (i=2; i <= number && number%i != 0; i++) {
+			}
+			
+			if (i == number) {
+				numOfPrimesFound++;
+			}
+			
+		}
+		
+		return number;
+		
+	}
+	
+}
